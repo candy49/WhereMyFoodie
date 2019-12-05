35d2a57d1a61f89700b90f38ad7c4c43fee2b493
Eriec Tan <eriectan88@gmail.com>
2019-12-05 18:25:32 +0700

    add form to welcome user

---
+++ WhereMyFoodie/Form4.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace WhereMyFoodie
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnUser = new System.Windows.Forms.Button();
+            this.btnAdmin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(131, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Food Pedia";
+            // 
+            // btnUser
+            // 
+            this.btnUser.Location = new System.Drawing.Point(136, 194);
+            this.btnUser.Name = "btnUser";
+            this.btnUser.Size = new System.Drawing.Size(75, 23);
+            this.btnUser.TabIndex = 1;
+            this.btnUser.Text = "user";
+            this.btnUser.UseVisualStyleBackColor = true;
+            this.btnUser.Click += new System.EventHandler(this.btnUser_Click);
+            // 
+            // btnAdmin
+            // 
+            this.btnAdmin.Location = new System.Drawing.Point(136, 233);
+            this.btnAdmin.Name = "btnAdmin";
+            this.btnAdmin.Size = new System.Drawing.Size(75, 23);
+            this.btnAdmin.TabIndex = 2;
+            this.btnAdmin.Text = "admin";
+            this.btnAdmin.UseVisualStyleBackColor = true;
+            this.btnAdmin.Click += new System.EventHandler(this.btnAdmin_Click);
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 339);
+            this.Controls.Add(this.btnAdmin);
+            this.Controls.Add(this.btnUser);
+            this.Controls.Add(this.label1);
+            this.Name = "Form4";
+            this.Text = "Form4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnUser;
+        private System.Windows.Forms.Button btnAdmin;
+    }
+}
---
+++ WhereMyFoodie/Form4.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WhereMyFoodie
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUser_Click(object sender, EventArgs e)
+        {
+            Form2 userForm = new Form2();
+            userForm.Show();
+        }
+
+        private void btnAdmin_Click(object sender, EventArgs e)
+        {
+            Form3 adminForm = new Form3();
+            adminForm.Show();
+        }
+    }
+}
